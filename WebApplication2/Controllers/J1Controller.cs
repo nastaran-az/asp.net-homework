2521da708314e06bee4eff4656bfea46331a82ba
nastaran azani <azaninastaran@gmail.com>
2020-10-15 23:05:33 -0400

    Asp.net homwork

---
+++ WebApplication2/Controllers/J1Controller.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebGrease.Css.Ast.Selectors;
+
+namespace WebApplication2.Controllers
+{
+    
+    public class J1Controller : ApiController
+    ///<summary>user choosse a burgur, a drink, a side and a dessert from menu by their number, and
+    ///program calculate their calories.</summary>
+    ///<param  name="burger">it is a number beetween 1 to 4 that will be index of burgercalori list</param>
+    ///<param name="drink">it is a number beetween 1 to 4 that will be index of drinkcalori list</param>
+    ///<param name="side">it is a number beetween 1 to 4 that will be index of sidecalori list</param>
+    ///<param name="side">it is a number beetween 1 to 4 that will be index of dessertcalori list</param>
+    ///<example>for example input is burger=4,drink=4, side=4, dessert=4-->countColori=0</example>
+    ///<example>for example input is burger=1,drink=2, side=3, dessert=4-->countColori=691</example>
+    {
+        [Route("api/J1/Menu/{burger}/{drink}/{side}/{dessert}")]
+        [HttpGet]
+
+        public string Menu(int burger, int drink, int side, int dessert)
+        {
+            List<int> burgercalorie = new List<int>() { 461, 431, 420, 0 }; // list for burgers' calorie
+            List<int> drinkcalorie = new List<int>() { 130, 160, 118, 0 };//list  for drinks'calorie
+            List<int> sidecalorie = new List<int>() { 100, 57, 70, 0 };//list  for sides'calorie
+            List<int> dessertcalorie = new List<int>() { 167, 266, 75, 0 };//list  for desserts'calorie
+            string errormessage = "please enter valid number between 1 to 4 ";
+            int countcalories = 0;
+            string message = "Your total calori count is ";
+            //if input was a number that there isn't in lists it shows a messsage
+            if ((burger <= 0) | (burger > 4) | (drink <= 0) | (drink > 4) | (side <= 0) | (side > 4) | (dessert<=0) | (dessert > 4))
+            {
+                return errormessage;
+            }
+            else
+            {
+                //since index of first item in a list is 0; therfore, when input parameter is 1 for access to first item
+               // program should param-1
+                burger -=1; 
+                drink -=1;
+                side -= 1;
+                dessert -= 1;
+                countcalories = burgercalorie[burger] + drinkcalorie[drink] + sidecalorie[side] + dessertcalorie[dessert];
+                message = message + countcalories;
+                return message;
+            }
+
+      }      
+
+    }
+}
---
+++ WebApplication2/Controllers/J2Controller.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication2.Controllers
+{
+    public class J2Controller : ApiController
+    {
+        ///<summary>
+        ///program recives 2 inputs that are sides of every dice,
+        ///and then calculates how many times sume of numbers on two  dices can be 10         
+        ///</summary>
+        ///<param name="side1">side1 is number of  sides in dice1 </param>
+        ///<param name="side2">side2 is number of  sides in dice2</param>
+        ///<example>side1=6 and side2=8 --> count=5 method return "There are 5 total ways to get the sum 10."</example>
+        ///<example>side1=12 and side2=4 --> count=4 method return "There are 4 total ways to get the sum 10."</example>
+        [Route("api/J2/DiceGame/{side1}/{side2}")]
+        [HttpGet]
+        public string DiceGame(int side1, int side2)
+        {
+            int count = 0;// variable for numbers of times that sum is 10
+            int i = 1;// for every side of the first dice it can be i=1,2,3,....,side1
+            string message;// variable to return final message
+            while (i <= side1)///for example i=1 then i sums j=1 to side2 if i+j==10 it will be added 1 to count                              
+            {
+                for (int j = 1; j <= side2; j++)//j is variable to labels of second dice j=1,2,3,...,side2
+                {
+                    if (i + j == 10)
+                    {
+                        count += 1;
+                    }
+                }
+                i++;
+
+            }
+            message = "There are " + count + " Totale ways to get the Sum 10";
+            return message;
+        }
+
+    }
+}
+
